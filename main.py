2801c0f8648f41a898c919d1dcf6ab59b5376939
yasmine <yasminee455@gmail.com>
2025-07-25 16:56:28 +0400

    random forest

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from sklearn.preprocessing import LabelEncoder, StandardScaler
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import RandomOverSampler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -35,23 +39,22 @@ one_hot_encoded[feature_cols] = scaler.fit_transform(one_hot_encoded[feature_col
 
 print(one_hot_encoded)
 
-dropout_counts = one_hot_encoded['DropoutRisk'].value_counts().sort_index()
-
 plt.figure(figsize=(6, 4))
-plt.bar(dropout_counts.index.astype(str), dropout_counts.values, width=0.4, color=['green', 'red'])
+plt.hist(one_hot_encoded['DropoutRisk'], bins=2, color='skyblue', edgecolor='black', rwidth=0.8)
 
 plt.xlabel('Dropout Risk (0 = No, 1 = Yes)')
 plt.ylabel('Number of Students')
-plt.title('Dropout Risk Distribution')
-plt.xticks(ticks=[0, 1], labels=['No Risk', 'At Risk'])
+plt.title('Dropout Risk Distribution (Histogram)')
+plt.xticks([0, 1], labels=['No Risk', 'At Risk'])
 plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 plt.tight_layout()
 plt.show()
 
 
+
 y = one_hot_encoded['DropoutRisk']
-X = one_hot_encoded.drop(['DropoutRisk', 'Underperform'], axis=1)
+X = one_hot_encoded.drop(['DropoutRisk', 'Underperform','StudentID', 'Name'], axis=1)
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -59,24 +62,23 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 oversampler = RandomOverSampler(random_state=42)
 X_train_oversampled, y_train_oversampled = oversampler.fit_resample(X_train, y_train)
 
-dropout_counts_over = Counter(y_train_oversampled)
-
-# Plot
 plt.figure(figsize=(6, 4))
-plt.bar(
-    [str(k) for k in dropout_counts_over.keys()],
-    dropout_counts_over.values(),
-    width=0.4,
-    color=['green', 'red']
-)
+plt.hist(y_train_oversampled, bins=2, color='skyblue', edgecolor='black', rwidth=0.8)
 
 plt.xlabel('Dropout Risk (0 = No, 1 = Yes)')
 plt.ylabel('Number of Students')
 plt.title('Dropout Risk Distribution After Oversampling')
-plt.xticks(ticks=[0, 1], labels=['No Risk', 'At Risk'])
+plt.xticks([0, 1], labels=['No Risk', 'At Risk'])
 plt.grid(axis='y', linestyle='--', alpha=0.7)
+
 plt.tight_layout()
 plt.show()
 
 
+rf = RandomForestClassifier(n_estimators=100, random_state=42)
 
+rf.fit(X_train, y_train)
+
+y_pred = rf.predict(X_test)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
