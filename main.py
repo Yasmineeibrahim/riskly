facfe6009e4a4d6a52d27d587530570b0134f46b
yasmine <yasminee455@gmail.com>
2025-07-25 16:26:01 +0400

    data oversampling

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from collections import Counter
 import numpy as np
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from imblearn.over_sampling import RandomOverSampler
 
 pd.set_option('display.max_rows', None)
 pd.set_option('display.max_columns', None)
@@ -45,3 +48,35 @@ plt.grid(axis='y', linestyle='--', alpha=0.7)
 
 plt.tight_layout()
 plt.show()
+
+
+y = one_hot_encoded['DropoutRisk']
+X = one_hot_encoded.drop(['DropoutRisk', 'Underperform'], axis=1)
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+oversampler = RandomOverSampler(random_state=42)
+X_train_oversampled, y_train_oversampled = oversampler.fit_resample(X_train, y_train)
+
+dropout_counts_over = Counter(y_train_oversampled)
+
+# Plot
+plt.figure(figsize=(6, 4))
+plt.bar(
+    [str(k) for k in dropout_counts_over.keys()],
+    dropout_counts_over.values(),
+    width=0.4,
+    color=['green', 'red']
+)
+
+plt.xlabel('Dropout Risk (0 = No, 1 = Yes)')
+plt.ylabel('Number of Students')
+plt.title('Dropout Risk Distribution After Oversampling')
+plt.xticks(ticks=[0, 1], labels=['No Risk', 'At Risk'])
+plt.grid(axis='y', linestyle='--', alpha=0.7)
+plt.tight_layout()
+plt.show()
+
+
+
