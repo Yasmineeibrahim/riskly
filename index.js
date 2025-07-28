d01998f443f2ee7529413b34d7b34dd9e5919cab
yasmine <yasminee455@gmail.com>
2025-07-28 14:33:51 +0400

    setting up project

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Server is running...");
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+dotenv.config();
+const PORT = process.env.PORT || 2000;
+
+app.get('/', (req, res) => {
+  res.send('Server is working!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
