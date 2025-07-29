702318e873c9d59b705a9da07e1586924b132b87
yasmine <yasminee455@gmail.com>
2025-07-29 09:18:50 +0400

    login page styling

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 dotenv.config();
 const PORT = process.env.PORT || 2000;
 
 app.get('/', (req, res) => {
-  res.send('Server is working!');
+  res.sendFile(path.join(__dirname, 'public', 'loginPage.html'));
 });
 
 app.listen(PORT, () => {
